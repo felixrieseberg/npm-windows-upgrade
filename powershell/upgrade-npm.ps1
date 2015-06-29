0e73b617bac897f34749cf2173fed899ccf18d6a
Felix Rieseberg <felix@felixrieseberg.com>
2015-06-29 11:06:37 -0700

    Basics
    - Get and display versions
    - UAC and Self-Elevation

---
+++ powershell/upgrade-npm.ps1
@@ -0,0 +1,44 @@
+# npm-windows-upgrade
+# This script updates npm on Windows, making sure that the npm installed
+# with `npm install -g npm@latest` is actually the first npm found in PATH
+# (C) Copyright 2015 Microsoft, developed by Felix Rieseberg
+# felix.rieseberg@microsoft.com
+
+# ----------------------------------------------------------------------
+# Usage: ./upgrade-npm.ps1 
+# ----------------------------------------------------------------------
+
+
+function IsAdministrator
+{
+    $Identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $Principal = New-Object System.Security.Principal.WindowsPrincipal($Identity)
+    $Principal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+
+
+function IsUacEnabled
+{
+    (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System).EnableLua -ne 0
+}
+
+#
+# Self-Elevate
+#
+if (!(IsAdministrator))
+{
+    if (IsUacEnabled)
+    {
+        [string[]]$argList = @('-NoProfile', '-NoExit', '-File', $MyInvocation.MyCommand.Path)
+        $argList += $MyInvocation.BoundParameters.GetEnumerator() | Foreach {"-$($_.Key)", "$($_.Value)"}
+        $argList += $MyInvocation.UnboundArguments
+        Start-Process PowerShell.exe -Verb Runas -WorkingDirectory $pwd -ArgumentList $argList
+        return
+    }
+    else
+    {
+        throw "You must be administrator to run this script"
+    }
+}
+
+
