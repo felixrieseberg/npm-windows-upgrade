032f4910608bedca19ca0deb44e750cee9913f37
Felix Rieseberg <felix@felixrieseberg.com>
2017-04-04 17:59:51 -0800

    :wrench: Use debug

--- src/debug.js
+++ src/debug.js
@@ -1,7 +1,10 @@
+const d = require('debug')('npm-windows-upgrade')
+
+// Ensure that all output is sent to stdout
+debug.log = console.log.bind(console)
+
 function debug (message) {
-  if (process.env.DEBUG) {
-    console.log(message)
-  }
+  return d(message)
 }
 
 module.exports = debug
